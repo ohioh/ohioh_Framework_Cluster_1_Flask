269ac0efff7d01b142d6a45fab2eb78680f3cac5
Codefred <alfredemmanuelinyang@gmail.com>
2020-06-19 03:22:01 +0100

    restrcutured

--- main/schemas/users.py
+++ main/schemas/users.py
@@ -5,8 +5,8 @@ import marshmallow.fields as ms_fields
 
 
 class UserSchema(Schema):
-    user_id = ms_fields.Str(required=True)
-    user_timestemp = ms_fields.DateTime(default=datetime.now(), required=True)
+    user_id = ms_fields.Str()
+    user_timestamp = ms_fields.DateTime(default=datetime.now())
     user_mac = ms_fields.Str(required=False)
     first_name = ms_fields.Str(required=False)
     last_name = ms_fields.Str(required=False)
@@ -15,12 +15,12 @@ class UserSchema(Schema):
     is_infected = ms_fields.Bool(required=True)
     infection_accuracy = ms_fields.Float(required=True)
     user_privacy_data = ms_fields.Bool(required=True)
-    tracking_save_duration = ms_fields.Float(required=True)
-    bluetooth_save_duration = ms_fields.Float(required=True)
-    data_save_duration = ms_fields.Float(required=True)
+    tracking_save_duration = ms_fields.Int(required=True)
+    bluetooth_save_duration = ms_fields.Int(required=True)
+    data_save_duration = ms_fields.Int(required=True)
     phone = ms_fields.Int(required=False)
     zip_code = ms_fields.Int(required=False)
-    registration_date = ms_fields.Date(default=datetime.now().date(), required=False)
+    registration_date = ms_fields.Date(default=datetime.now().date())
 
     class Meta:
         unknown = EXCLUDE
