f1f8d6fe747217c0a3edf13e05964f0117a34a00
Codefred <alfredemmanuelinyang@gmail.com>
2020-06-19 02:45:01 +0100

    restructure code to API DOC

--- main/schemas/users.py
+++ main/schemas/users.py
@@ -1,24 +1,26 @@
-import datetime
+from datetime import datetime
 
 from marshmallow import Schema, EXCLUDE
 import marshmallow.fields as ms_fields
 
 
 class UserSchema(Schema):
-    user_id = ms_fields.Str()
-    first_name = ms_fields.Str()
-    last_name = ms_fields.Str()
-    age = ms_fields.Int()
-    country=ms_fields.Str()
-    is_infected = ms_fields.Bool()
-    infection_accuracy = ms_fields.Float()
-    user_privacy_data = ms_fields.Bool()
-    tracking_save_duration = ms_fields.Float()
-    bluetooth_save_duration = ms_fields.Float()
-    data_save_duration = ms_fields.Float()
-    phone = ms_fields.Int()
-    zip_code = ms_fields.Int()
-    registration_date_time = ms_fields.DateTime(default=datetime.datetime.now())
+    user_id = ms_fields.Str(required=True)
+    user_timestemp = ms_fields.DateTime(default=datetime.now(), required=True)
+    user_mac = ms_fields.Str(required=False)
+    first_name = ms_fields.Str(required=False)
+    last_name = ms_fields.Str(required=False)
+    age = ms_fields.Int(required=False)
+    country=ms_fields.Str(required=False)
+    is_infected = ms_fields.Bool(required=True)
+    infection_accuracy = ms_fields.Float(required=True)
+    user_privacy_data = ms_fields.Bool(required=True)
+    tracking_save_duration = ms_fields.Float(required=True)
+    bluetooth_save_duration = ms_fields.Float(required=True)
+    data_save_duration = ms_fields.Float(required=True)
+    phone = ms_fields.Int(required=False)
+    zip_code = ms_fields.Int(required=False)
+    registration_date = ms_fields.Date(default=datetime.now().date(), required=False)
 
     class Meta:
         unknown = EXCLUDE
