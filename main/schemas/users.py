be1983c9567674fff6f0b263f3f8932171f9996a
Codefred <alfredemmanuelinyang@gmail.com>
2020-06-21 05:29:47 +0100

    configuring deleting sessions

--- main/schemas/users.py
+++ main/schemas/users.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date, timedelta
 
 from marshmallow import Schema, EXCLUDE
 import marshmallow.fields as ms_fields
@@ -7,20 +7,22 @@ import marshmallow.fields as ms_fields
 class UserSchema(Schema):
     user_id = ms_fields.Str()
     user_timestamp = ms_fields.DateTime(default=datetime.now())
-    user_mac = ms_fields.Str(required=False)
-    first_name = ms_fields.Str(required=False)
-    last_name = ms_fields.Str(required=False)
-    age = ms_fields.Int(required=False)
-    country=ms_fields.Str(required=False)
-    is_infected = ms_fields.Bool(required=True)
-    infection_accuracy = ms_fields.Float(required=True)
-    user_privacy_data = ms_fields.Bool(required=True)
-    tracking_save_duration = ms_fields.Int(required=True)
-    bluetooth_save_duration = ms_fields.Int(required=True)
-    data_save_duration = ms_fields.Int(required=True)
+    user_mac = ms_fields.Str(default="")
+    first_name = ms_fields.Str(default="")
+    last_name = ms_fields.Str(default="")
+    age = ms_fields.Int(required=False, default="")
+    country=ms_fields.Str(required=False, default="")
+    is_infected = ms_fields.Bool(default=False)
+    infection_accuracy = ms_fields.Float(default=0.0)
+    user_privacy_data = ms_fields.Bool(default=False)
+    tracking_save_duration = ms_fields.Int(default=14)
+    bluetooth_save_duration = ms_fields.Int(default=14)
+    data_save_duration = ms_fields.Int(default=90)
+    data_delete_date = ms_fields.Date()
     phone = ms_fields.Int(required=False)
     zip_code = ms_fields.Int(required=False)
-    registration_date = ms_fields.Date(default=datetime.now().date())
+    registration_date = ms_fields.Date(default=date.today())
+
 
     class Meta:
         unknown = EXCLUDE
--- main/services/db.py
+++ main/services/db.py
@@ -1,6 +1,8 @@
 from flask import make_response
 from main.resources.message_templates import error_message
 from bson.objectid import ObjectId
+from bson import json_util
+from  datetime import datetime, date, timedelta
 
 class DbOperations:
     def __init__(self, collections, schema):
@@ -8,14 +10,19 @@ class DbOperations:
         self.schema = schema
 
     def insert(self, payload):
+        new_date = date.today() + timedelta(days=payload['data_save_duration'])
         payload = self.schema().load(payload)
+        payload['data_delete_date'] = new_date.isoformat()
+        payload['user_timestamp'] = datetime.now().isoformat()
+        payload['registration_date'] = date.today().isoformat()
+
         inserted_id = self.collections.insert_one(payload).inserted_id
 
         self.update(
             criteria={ '_id': ObjectId(inserted_id) },
-            update={ 'user_id': str(inserted_id) }
+            update={'user_id': str(inserted_id)}
         )
-        return str(inserted_id)
+        return f"{str(inserted_id)} added"
 
     def find_one(self, criteria):
         record = self.collections.find_one(criteria)
@@ -23,15 +30,15 @@ class DbOperations:
         return make_response(result)
 
     def find_all(self):
-        
         cursor = self.collections.find()
-        result = self.schema(many=True).dumps(cursor)
-        return make_response(result)
+        result = self.schema(many=True).load(cursor)
+        return make_response(str(result))
 
     def update(self, criteria, update):
         new_value = { "$set": update }
         result = self.collections.update_one(criteria, new_value, upsert=True).modified_count
         return result
+
 
     def delete(self, criteria):
         result = self.collections.delete_one(criteria)
